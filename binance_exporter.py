0aba606341245292eec4ae69116e6f4d3d74047b
Thomas GUIRRIEC <thomas@guirriec.fr>
2023-11-04 15:36:09 +0000

    Update binance_exporter.py

--- binance_exporter.py
+++ binance_exporter.py
@@ -125,7 +125,7 @@ except ValueError:
 # Check Mandatory Environment Variable
 for var in MANDATORY_ENV_VARS:
     if var not in os.environ:
-        logging.critical("%s environement variable must be set !", var)
+        logging.critical("%s environment variable must be set !", var)
         os._exit(1)
 
 BINANCE_KEY = os.environ.get("BINANCE_KEY")
